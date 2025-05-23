f12f21f93a5cedd520168c846d7d267ab7c0f6d2
khans8890 <kansumaira@gmail.com>
2025-05-23 01:02:32 -0400

    adding imports to main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 )
